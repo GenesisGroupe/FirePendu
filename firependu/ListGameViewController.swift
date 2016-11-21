3eb24d538be80815677e0f07aee2e3c92c91b4b8
Julien Hennet <julien.hennet@itelios.com>
2016-11-21 14:14:32 +0100

    creating view controllers

---
+++ firependu/ListGameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListGameViewController.swift
+//  firependu
+//
+//  Created by Julien Hennet on 21/11/2016.
+//
+//
+
+import UIKit
+
+class ListGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
