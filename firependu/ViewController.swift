df3a55b391ac51c1bfa3738ac9caa5e7b195d161
Julien Hennet <j.hennet@genesis-groupe.com>
2016-11-21 09:53:40 +0100

    Initial Commit

---
+++ firependu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firependu
+//
+//  Created by Julien Hennet on 21/11/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
